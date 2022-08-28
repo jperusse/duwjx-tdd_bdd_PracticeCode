da6a7c140948ae6a17b0b095980ef5f6eba8a103
Jim Perusse <67986763+jperusse@users.noreply.github.com>
2022-08-28 14:08:02 -0700

    complete lab

---
+++ inc_dec.py
@@ -0,0 +1,9 @@
+""" Module to increment/decriment a number """
+
+def increment(value):
+    """ method to increment value """
+    return value + 1
+
+def decrement(value):
+    """ method to decrement value """
+    return value - 1
